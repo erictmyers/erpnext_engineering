41af062652efd15824992f3fc671d65bbfc73d63
Eric Myers <eric.t.myers@gmail.com>
2020-10-03 16:13:38 -0500

    Adding Item customizations

---
+++ erpnext_engineering/engineering/doctype/item/item.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, Ability Engineering and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Item', {
+	// refresh: function(frm) {
+
+	// }
+});
